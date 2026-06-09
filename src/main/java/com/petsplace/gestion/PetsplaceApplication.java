67b8e240deaa85da683b26504d2543c6dd535ae1
Juan Colqui <colquijuan51@gmail.com>
2026-06-08 23:51:28 -0500

    ESTRUCTURA INICIAL DE NUESTRO PROYECTO

---
+++ src/main/java/com/petsplace/gestion/PetsplaceApplication.java
@@ -0,0 +1,13 @@
+package com.petsplace.gestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetsplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetsplaceApplication.class, args);
+	}
+
+}
